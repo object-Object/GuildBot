7ce0a993949b3e387082515a30ae0d8e47641472
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-08-09 01:15:30 +0000

    add extension management commands

--- extensions/owner.py
+++ extensions/owner.py
@@ -1,16 +1,74 @@
+import discord
+import os
 from discord.ext import commands
 
 class Owner(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.group(invoke_without_command=True)
+	@commands.group(invoke_without_command=True, aliases=["ext"])
 	@commands.is_owner()
 	async def extensions(self, ctx):
-		prefix = self.bot.command_prefix
-		await ctx.send(f"""`{prefix}extensions load <extension>` Loads specified extension
-`{prefix}extensions unload <extension>` Unloads specified extension
-`{prefix}extensions reload <extension>` Reloads specified extension""")
+		await ctx.send(embed=discord.Embed(
+			title="List of extensions",
+			description="```\n" + "\n".join(sorted([f[:-3] for f in os.listdir("extensions") if f.endswith(".py")])) + "```",
+			color=discord.Color(0x00ff00)
+		))
+
+	@extensions.command()
+	@commands.is_owner()
+	async def load(self, ctx, extension: str):
+		try:
+			self.bot.load_extension("extensions." + extension)
+		except commands.ExtensionError as e:
+			await ctx.send(embed=discord.Embed(
+				description=f"Failed to load extension `{extension}`!```\n" + str(e) + "```",
+				color=discord.Color(0xff0000)
+			))
+		else:
+			await ctx.send(embed=discord.Embed(
+				description=f"Successfully loaded extension `{extension}`.",
+				color=discord.Color(0x00ff00)
+			))
+
+	@extensions.command()
+	@commands.is_owner()
+	async def unload(self, ctx, extension: str):
+		if extension=="owner":
+			await ctx.send(embed=discord.Embed(
+				description=f"Extension `owner` cannot be unloaded!",
+				color=discord.Color(0xff0000)
+			))
+			return
+
+		try:
+			self.bot.unload_extension("extensions." + extension)
+		except commands.ExtensionError as e:
+			await ctx.send(embed=discord.Embed(
+				description=f"Failed to unload extension `{extension}`!```\n" + str(e) + "```",
+				color=discord.Color(0xff0000)
+			))
+		else:
+			await ctx.send(embed=discord.Embed(
+				description=f"Successfully unloaded extension `{extension}`.",
+				color=discord.Color(0x00ff00)
+			))
+
+	@extensions.command(aliases=["r"])
+	@commands.is_owner()
+	async def reload(self, ctx, extension: str):
+		try:
+			self.bot.reload_extension("extensions." + extension)
+		except commands.ExtensionError as e:
+			await ctx.send(embed=discord.Embed(
+				description=f"Failed to reload extension `{extension}`!```\n" + str(e) + "```",
+				color=discord.Color(0xff0000)
+			))
+		else:
+			await ctx.send(embed=discord.Embed(
+				description=f"Successfully reloaded extension `{extension}`.",
+				color=discord.Color(0x00ff00)
+			))
 
 def setup(bot):
 	bot.add_cog(Owner(bot))
