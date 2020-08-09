dfd294479d6056430d0501c07f164b2b1f5dd57c
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-08-09 01:15:03 +0000

    add error handling for NotOwner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import config
+import discord
+from discord.ext import commands
 
 from bot import Bot
 
@@ -7,5 +9,14 @@ bot = Bot(command_prefix=config.prefix)
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}.")
+
+@bot.event
+async def on_command_error(ctx, error):
+	if isinstance(error, commands.NotOwner):
+		await ctx.send(embed=discord.Embed(
+			title="You may not run this command!",
+			description="You do not own this bot.",
+			color=discord.Color(0xff0000)
+		))
 
 bot.run(config.token)
