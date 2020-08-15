f26343c9733dd6d41797e09406f833aee874737b
Bluenix <gabriel.nordholm@gmail.com>
2020-08-15 14:53:25 +0200

    Change to asyncpg

--- bot.py
+++ bot.py
@@ -3,18 +3,12 @@ from discord.ext import commands
 import sys
 import traceback
 import os
-import sqlite3
 import config
 
 class Bot(commands.Bot):
     def __init__(self, conn, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.conn = conn
-
-        # create db tables if they don't exist already
-        c = self.conn.cursor()
-        c.execute("CREATE TABLE IF NOT EXISTS threads (channel_id, author_id)")
-        conn.commit()
 
         for extension in [f for f in os.listdir("extensions") if f.endswith(".py")]:
             try:
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import config
 import discord
 from discord.ext import commands
-import sqlite3
 
 from bot import Bot
 from utils.help import GuildBotHelp
 
-bot = Bot(conn=sqlite3.connect(config.db), command_prefix=config.prefix, help_command=GuildBotHelp())
+bot = Bot(command_prefix=config.prefix, help_command=GuildBotHelp())
 
 @bot.event
 async def on_ready():
