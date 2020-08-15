e8402bb0770e53036331d6bb13ad68845348f809
Bluenix <gabriel.nordholm@gmail.com>
2020-08-15 22:11:51 +0200

    Fix db.sh install file
    Now successfully installs and sets up database and user account for PostgreSQL

--- db.sh
+++ db.sh
@@ -1,7 +1,10 @@
 echo "Checking if all packages are up to date"
 sudo apt-get update
-echo "Installing postgreSQL and postgreSQL-contrib."
+sudo apt-get upgrade
+echo "Installing postgreSQL and postgreSQL-contrib"
 sudo apt-get install postgresql postgresql-contrib
-echo "Creating the user and database in PostgreSQL"
-sudo -u postgres psql -c "CREATE USER guildbot WITH PASSWORD 'gu1ldb0t'; ALTER USER guildbot WITH SUPERUSER; CREATE DATABASE guildbot; \q;"
-echo "Installed and established database"
+echo "Starting postgres server in case of it being offline"
+sudo service postgresql start
+echo "Creating the user and database"
+sudo -u postgres psql -c "CREATE USER guildbot WITH PASSWORD 'gu1ldb0t'; ALTER USER guildbot WITH SUPERUSER;" -c "CREATE DATABASE guildbot;"
+echo "Installed and established database and user. Finished!"
