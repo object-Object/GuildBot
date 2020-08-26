73353b24ed9e7685923729d60b88fe73d947b109
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-08-26 18:34:38 +0000

    create functions for handling settings

--- extensions/settings.py
+++ extensions/settings.py
@@ -1,7 +1,95 @@
 import discord
 from discord.ext import commands
-from utils import converters, checks
+from utils import converters, checks, errors
 import typing
+import inspect
+
+display_lookup = {
+    discord.Role: "mention",
+    discord.CategoryChannel: "name",
+    discord.TextChannel: "mention",
+}
+
+def display(obj):
+    for k,v in display_lookup.items():
+        if isinstance(obj, k):
+            return getattr(obj, v)
+    raise Exception(f"Type {type(obj)} not found in display_lookup")
+
+async def set_single_setting(ctx, setting_key, old_obj, new_obj):
+    if new_obj is None:
+        await ctx.send(embed=discord.Embed(
+            title="Current value",
+            description=f"`{setting_key}` is currently {display(old_obj) if old_obj else 'not set'}.",
+            color=discord.Color(0x007fff)))
+    else:
+        setattr(ctx.bot.settings, setting_key, new_obj.id)
+        ctx.bot.settings.save()
+        await ctx.send(embed=discord.Embed(
+            title="Updated value",
+            description=f"`{setting_key}` has been {f'changed from {display(old_obj)}' if old_obj else 'set'} to {display(new_obj)}.",
+            color=discord.Color(0x007fff)))
+
+async def show_list_setting(ctx, setting_key, get_func):
+    obj_displays = []
+    for obj_id in getattr(ctx.bot.settings, setting_key):
+        try:
+            obj_displays.append(display(get_func(obj_id)))
+        except AttributeError:
+            pass
+    await ctx.send(embed=discord.Embed(
+        title="Current value",
+        description=f"The current values of `{setting_key}` are: {', '.join(obj_displays) if obj_displays else '(none set)'}.",
+        color=discord.Color(0x007fff)))
+
+async def add_to_list_setting(ctx, setting_key, objs):
+    obj_displays = []
+    to_add = []
+    invalid_obj_displays = []
+    setting = getattr(ctx.bot.settings, setting_key)
+    for obj in objs:
+        if obj.id in setting:
+            invalid_obj_displays.append(display(obj))
+        else:
+            obj_displays.append(display(obj))
+            to_add.append(obj.id)
+    if invalid_obj_displays:
+        await ctx.send(embed=discord.Embed(
+            title="Command failed!",
+            description=f"The following values were already in `{setting_key}`: {', '.join(invalid_obj_displays)}.",
+            color=discord.Color(0xff0000)))
+    else:
+        setting += to_add
+        ctx.bot.settings.save()
+        await ctx.send(embed=discord.Embed(
+            title="Updated value",
+            description=f"The following values have been added to `{setting_key}`: {', '.join(obj_displays)}.",
+            color=discord.Color(0x007fff)))
+
+async def remove_from_list_setting(ctx, setting_key, objs):
+    obj_displays = []
+    to_remove = []
+    invalid_obj_displays = []
+    setting = getattr(ctx.bot.settings, setting_key)
+    for obj in objs:
+        if obj.id not in setting:
+            invalid_obj_displays.append(display(obj))
+        else:
+            obj_displays.append(display(obj))
+            to_remove.append(obj.id)
+    if invalid_obj_displays:
+        await ctx.send(embed=discord.Embed(
+            title="Command failed!",
+            description=f"The following values were not in `{setting_key}`: {', '.join(invalid_obj_displays)}.",
+            color=discord.Color(0xff0000)))
+    else:
+        for obj_id in to_remove:
+            setting.remove(obj_id)
+        ctx.bot.settings.save()
+        await ctx.send(embed=discord.Embed(
+            title="Updated value",
+            description=f"The following values have been removed from `{setting_key}`: {', '.join(obj_displays)}.",
+            color=discord.Color(0x007fff)))
 
 class Settings(commands.Cog):
     def __init__(self, bot):
@@ -9,110 +97,51 @@ class Settings(commands.Cog):
 
     @commands.command(aliases=["trole"])
     @commands.guild_only()
-    @checks.trustee_only()
-    async def trusteerole(self, ctx, role: typing.Optional[converters.RoleConverter]):
-        if role is None:
-            current_role=ctx.guild.get_role(ctx.bot.settings.trustee_role)
-            await ctx.send(embed=discord.Embed(
-                title="Current value",
-                description=f"The Trustee role is currently {current_role.mention if current_role else 'not set'}.",
-                color=discord.Color(0x007fff)))
-        else:
-            ctx.bot.settings.trustee_role=role.id
-            ctx.bot.settings.save()
-            await ctx.send(embed=discord.Embed(
-                title="Updated value",
-                description=f"The Trustee role has been set to {role.mention}.",
-                color=discord.Color(0x007fff)))
+    @checks.is_guild_owner()
+    async def trusteerole(self, ctx, new_role: typing.Optional[converters.RoleConverter]):
+        old_role = ctx.guild.get_role(ctx.bot.settings.trustee_role)
+        if old_role is not None and new_role is not None:
+            raise errors.CommandFailed("`trustee_role` cannot be changed once set.")
+        await set_single_setting(ctx, "trustee_role", old_role, new_role)
+
 
     @commands.command(aliases=["archivecat"])
     @commands.guild_only()
     @checks.trustee_only()
-    async def archivecategory(self, ctx, category: typing.Optional[converters.CategoryConverter]):
-        if category is None:
-            current_category=ctx.guild.get_channel(ctx.bot.settings.archive_category)
-            await ctx.send(embed=discord.Embed(
-                title="Current value",
-                description=f"The archive category is currently {'`'+current_category.name+'`' if current_category else 'not set'}.",
-                color=discord.Color(0x007fff)))
-        else:
-            ctx.bot.settings.archive_category=category.id
-            ctx.bot.settings.save()
-            await ctx.send(embed=discord.Embed(
-                title="Updated value",
-                description=f"The archive category has been set to `{category.name}`.",
-                color=discord.Color(0x007fff)))
+    async def archivecategory(self, ctx, new_category: typing.Optional[converters.CategoryConverter]):
+        old_category = ctx.guild.get_channel(ctx.bot.settings.archive_category)
+        await set_single_setting(ctx, "archive_category", old_category, new_category)
+
 
     @commands.group(aliases=["threadcats"], invoke_without_command=True)
     @commands.guild_only()
     @checks.trustee_only()
     async def threadcategories(self, ctx):
-        category_names = []
-        for category_id in ctx.bot.settings.thread_categories:
-            try:
-                category_names.append(ctx.guild.get_channel(category_id).name)
-            except AttributeError:
-                pass
-        await ctx.send(embed=discord.Embed(
-            title="Current value",
-            description=f"The current thread categories are: {'`'+'`, `'.join(category_names)+'`' if category_names else 'not set'}.",
-            color=discord.Color(0x007fff)))
+        await show_list_setting(ctx, "thread_categories", ctx.guild.get_channel)
 
     @threadcategories.command()
     @commands.guild_only()
     @checks.trustee_only()
     async def add(self, ctx, *categories: commands.Greedy[converters.CategoryConverter]):
-        category_names = []
-        for category in categories:
-            category_names.append(category.name)
-            ctx.bot.settings.thread_categories.append(category.id)
-        ctx.bot.settings.save()
-        await ctx.send(embed=discord.Embed(
-            title="Updated value",
-            description=f"The following have been added to the thread categories: `{'`, `'.join(category_names)}`.",
-            color=discord.Color(0x007fff)))
+        if not categories:  # this is needed because varargs by default are fine with having 0 arguments
+            raise commands.MissingRequiredArgument(ctx.command.clean_params["categories"])
+        await add_to_list_setting(ctx, "thread_categories", categories)
 
     @threadcategories.command()
     @commands.guild_only()
     @checks.trustee_only()
     async def remove(self, ctx, *categories: commands.Greedy[converters.CategoryConverter]):
-        category_names = []
-        invalid_category_names = []
-        for category in categories:
-            try:
-                ctx.bot.settings.thread_categories.remove(category.id)
-                category_names.append(category.name)
-            except ValueError:
-                invalid_category_names.append(category.name)
-        if invalid_category_names:
-            await ctx.send(embed=discord.Embed(
-                title="Command failed!",
-                description=f"The following were not in the thread categories: `{'`, `'.join(invalid_category_names)}`.",
-                color=discord.Color(0xff0000)))
-        else:
-            ctx.bot.settings.save()
-            await ctx.send(embed=discord.Embed(
-                title="Updated value",
-                description=f"The following have been removed from the thread categories: `{'`, `'.join(category_names)}`.",
-                color=discord.Color(0x007fff)))
+        if not categories:  # this is needed because varargs by default are fine with having 0 arguments
+            raise commands.MissingRequiredArgument(ctx.command.clean_params["categories"])
+        await remove_from_list_setting(ctx, "thread_categories", categories)
+
 
     @commands.command(aliases=["welcomechan"])
     @commands.guild_only()
     @checks.trustee_only()
-    async def welcomechannel(self, ctx, channel: typing.Optional[converters.TextChannelConverter]):
-        if channel is None:
-            current_channel=ctx.guild.get_channel(ctx.bot.settings.welcome_channel)
-            await ctx.send(embed=discord.Embed(
-                title="Current value",
-                description=f"The welcome channel is currently {current_channel.mention if current_channel else 'not set'}.",
-                color=discord.Color(0x007fff)))
-        else:
-            ctx.bot.settings.welcome_channel=channel.id
-            ctx.bot.settings.save()
-            await ctx.send(embed=discord.Embed(
-                title="Updated value",
-                description=f"The welcome channel has been set to {channel.mention}.",
-                color=discord.Color(0x007fff)))
+    async def welcomechannel(self, ctx, new_channel: typing.Optional[converters.TextChannelConverter]):
+        old_channel = ctx.guild.get_channel(ctx.bot.settings.welcome_channel)
+        await set_single_setting(ctx, "welcome_channel", old_channel, new_channel)
 
 # Load extension
 def setup(bot):
