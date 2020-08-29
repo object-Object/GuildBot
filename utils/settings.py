fb5f5fe484677fab82cec3ef768e52e64830d8dc
Bluenix <gabriel.nordholm@gmail.com>
2020-08-29 15:17:24 +0200

    Redesign save and loading of settings

--- utils/settings.py
+++ utils/settings.py
@@ -1,33 +1,16 @@
-import copy
 import json
 
 import config
 
-default_settings = {  # key: setting name, value: default value
-    "thread_categories": [],
-    "autoroles": [],
-    "archive_category": None,
-    "trustee_role": None,
-    "welcome_channel": None,
-    }
-
 
 class Settings():
-    def __init__(self):
-        try:
+    def __init__(self, settings=None):
+        if settings is None:
             with open(config.settings_filename, "r") as f:
-                file_json = json.load(f)
-        except OSError:  # file doesn't exist yet
-            file_json = copy.deepcopy(default_settings)
-            with open(config.settings_filename, "w") as f:
-                json.dump(file_json, f)
-        for s in default_settings:
-            setattr(self, s, file_json.pop(s))
-        if file_json:  # file_json isn't empty after popping all the valid settings
-            raise Exception(f"Unexpected entries in {config.settings_filename}")
+                settings = json.load(f)
+        self.__dict__.update(settings)
 
-    def save(self):
-        """Write the current setting values to the json file."""
-        file_json = {s: getattr(self, s) for s in default_settings}
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value
         with open(config.settings_filename, "w") as f:
-            json.dump(file_json, f)
+            json.dump(self.__dict__, f)
