c82d41d8ed5d4889f82b260353476d4707c4a642
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-10-08 17:50:59 +0000

    bump d.py to 1.5.0 and add intents

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import config
+import discord
 from bot import Bot
 from utils.help import GuildBotHelp
 
-bot = Bot(command_prefix=config.prefix, help_command=GuildBotHelp())
+intents = discord.Intents.default()
+intents.members = True
+
+bot = Bot(command_prefix=config.prefix, help_command=GuildBotHelp(), intents=intents)
 
 
 @bot.event
